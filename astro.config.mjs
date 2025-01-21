249a4d89155e3b4b061c8e3de1a1c4b6ba3f9244
Santiago Garreta <santiagogarreta6@gmail.com>
2025-01-21 15:37:09 -0300

    deps

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ export default defineConfig({
   base: '/SUYpport-page/',
   experimental: {
     viewTransitions: true,
+    optimizeDeps: true, 
   },
   integrations: [tailwind(), compress()],
 });
